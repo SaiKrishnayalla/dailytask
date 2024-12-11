303e8fc9795b5835939afdfe30593cfcf20f06ab
Saikrishna <yallasaikrishnavamsi@gmail.com>
2024-12-11 14:49:18 +0530

    sets

---
+++ sets.py
@@ -0,0 +1,15 @@
+set_1 = {2,3,True,'vamsi',2,3,4,5,6,False}
+print(set_1)
+
+set={1}
+print(set)
+print(type(set))
+
+set_1.add(20)
+print(set_1)
+
+print(4 in set_1)
+print (20 in set_1)
+
+set_1.remove('vamsi')
+print(set_1)
