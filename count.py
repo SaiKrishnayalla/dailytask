4f4a7421a8a6fb6411b07f4243dfe62aaabf9ebe
Saikrishna <yallasaikrishnavamsi@gmail.com>
2024-12-11 15:09:25 +0530

    practice

---
+++ count.py
@@ -0,0 +1,3 @@
+x = eval(input("enter the tuple: "))
+y = int(input("enter the number: " ))
+print(x.count(y))
---
+++ tuple.py
@@ -0,0 +1,13 @@
+# write a python program to add multiple tuples and remove them 
+tup_1 = (1,2,3,4,5)
+print(type(tup_1))
+tup_2 =(6,7)
+print(tup_2 + tup_1)
+x = tup_2[1]
+print(x)
+
+#slicing in tuple
+y =tup_1[::-1]
+print(y)
+y = tup_1[1:5]
+print(y)
